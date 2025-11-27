83c3be24458ec95f17ff4c48e6e6f2a083bf78ed
Christins <17354303956@163.com>
2025-11-27 20:04:39 +0800

    chore: 移除无用代码

--- src/pages/admin/AdminLayout.tsx
+++ src/pages/admin/AdminLayout.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useLocation, Outlet, useNavigate } from 'react-router-dom';
 import {
     FaHome,
