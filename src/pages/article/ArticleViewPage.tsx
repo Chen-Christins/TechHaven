30cc39a03a6183c10e524f6d6f255e5f2f5954d7
Christins <chen.christins@qq.com>
2026-05-15 22:05:22 +0800

    feat: update author stats in ArticleViewPage to use dynamic article data

--- src/pages/article/ArticleViewPage.tsx
+++ src/pages/article/ArticleViewPage.tsx
@@ -88,9 +88,9 @@ const ArticleViewPage: React.FC = () => {
                   authorId={article.user_id}
                   articleId={article.id}
                   authorStats={{
-                    articles: 24,
-                    followers: 1284,
-                    likes: 342,
+                    articles: article.author_article_count ?? 0,
+                    followers: article.author_follower_count ?? 0,
+                    likes: article.author_praise_count ?? 0,
                   }}
                   views={article.views ?? 0}
                   praises={article.praise ?? article.praises ?? 0}
